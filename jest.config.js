dea0580fd3ebce960e212ef498333c3fdd73ac53
fabianmoronzirfas <fabianmoronzirfas@protonmail.ch>
2020-09-28 15:55:26 +0200

    add pg to test runner

--- jest.config.js
+++ jest.config.js
@@ -2,6 +2,12 @@
 const config = require("@inpyjamas/scripts/dist/config/jest/typescript");
 const { merge } = require("@inpyjamas/scripts/dist/utlities/merge");
 module.exports = merge(config, {
+  preset: "ts-jest",
+  globals: {
+    "ts-jest": {
+      diagnostics: { warnOnly: true },
+    },
+  },
   collectCoverageFrom: ["src/**/*.{ts}", "!src/test/**/*"],
   modulePathIgnorePatterns: ["<rootDir>/dist"],
 });
